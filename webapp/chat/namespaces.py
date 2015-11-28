0b9d73b42c3c58aa5e52ae605afd5f71f6ae2344
Baatarsuren Munkhdorj <munkhdorj.bs@gmail.com>
2015-11-28 18:28:17 +0900

    reform: socket interface

--- webapp/chat/namespaces.py
+++ webapp/chat/namespaces.py
@@ -5,6 +5,7 @@ from chat import app
 
 class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
     nicknames = []
+	sensor_data = []
 
     def initialize(self):
         self.logger = app.logger
@@ -22,7 +23,7 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
         self.log('Nickname: {0}'.format(nickname))
         self.nicknames.append(nickname)
         self.session['nickname'] = nickname
-        self.broadcast_event('announcement', '%s has connected' % nickname)
+        self.broadcast_event('announcement', nickname)
         self.broadcast_event('nicknames', self.nicknames)
         return True, nickname
 
@@ -31,7 +32,7 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
         self.log('Disconnected')
         nickname = self.session['nickname']
         self.nicknames.remove(nickname)
-        self.broadcast_event('announcement', '%s has disconnected' % nickname)
+        self.broadcast_event('announcement', nickname)
         self.broadcast_event('nicknames', self.nicknames)
         self.disconnect(silent=True)
         return True
@@ -41,3 +42,11 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
         self.emit_to_room(self.room, 'msg_to_room',
             self.session['nickname'], msg)
         return True
+
+	def recv_data(self, data):
+		# Send sensor data.
+		self.log('User data: {0}'.format(data))
+		self.sensor_data.append(data);
+		self.emit_to_room(self.room, 'sensor_data', 
+			self.session['nickname', data])
+		return True
