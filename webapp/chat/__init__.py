fa0f01fd733016df7516568d1ea89bac8af5248b
byambajav <byambajav.n@gmail.com>
2015-11-29 04:56:36 +0900

    Add login only (signup is not present yet)

--- webapp/chat/__init__.py
+++ webapp/chat/__init__.py
@@ -2,17 +2,22 @@ from gevent import monkey
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+import flask.ext.login as flask_login
 
 monkey.patch_all()
 
 app = Flask(__name__)
 app.debug = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/chat.db'
+app.secret_key = 'Xji8NKizj7ZZYFRE5vpnKw/3BN/Nw0qwVjqAqHK6VjY='
+
 db = SQLAlchemy(app)
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
 
 import chat.views
 import db_tools
-
 
 def init_db():
     db_tools.drop_all_tables()
@@ -22,3 +27,14 @@ def init_db():
 
 if __name__ == '__main__':
     app.run()
+
+
+from chat.models import User
+
+
+@login_manager.user_loader
+def user_loader(email):
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return
+    return user
