4c4ce27c0eeeba6dc0a6c6c4672072f508e1820f
Batchunag Dashdemberel <gtokyo2010@gmail.com>
2015-11-29 09:47:44 +0900

    Login working properly

--- webapp/chat/static/js/bonhomie.js
+++ webapp/chat/static/js/bonhomie.js
@@ -1,34 +1,13 @@
 var total = 0;
-var types = ["grandma", "dad", "mom", "son", "daughter"];
+// var types = ["grandma", "dad", "mom", "son", "daughter"];
 var emos = ["angry", "sad", "happy", "normal", "scared"];
 var colors = ["#B4E0DC", "#1DAFEC", "#9CD09C", "#A18981", "#F3896B"];
 var atms = []
 var images = ["static/img/winter_wp1.jpg", "static/img/winter_wp2.jpg", "static/img/winter_wp3.jpg", "static/img/summer_wp1.jpg", "static/img/summer_wp2.png", "static/img/summer_wp3.jpg"];
 
-$qtable = $("<table>");
-$qdiv = [];
-for (var i=1; i<6; i++){
-	$qa = $("<a>", {href:"#", id: "img"+i}).append($("<img>", {src: "static/img/"+types[i-1]+"_normal.png"}));
-	$qa.click(function(){
-		// console.log($(this).attr('id'));
-	});
-	$qdiv[i] = $("<div>", {class: "biliboard electronic col"+i})
-		.append($qa);
-}
-$qtable.append($("<tr>", {style: "width:100%"})
-	.append($("<td>").append($qdiv[1]))
-	.append($("<td>").append($qdiv[2])))
-.append($("<tr>", {style: "width:100%"})
-	.append($("<td>").append($qdiv[3]))
-	.append($("<td>").append($qdiv[4])))
-.append($("<tr>", {style: "width:100%"})
-	.append($("<td>").append($qdiv[5])));
-
-// console.log($qtable);
-// $("#modalTable").html($qtable);
-
 
 var render_user = function(user){
+	if (ms[user]=='house') return;
 	var bit = user%2;
 	var side = bit ? 'left': 'right';
 	var sideDiv = 'col-xs-6 pull-' + side; 
@@ -37,7 +16,7 @@ var render_user = function(user){
 	var $div = [];
 	var $person = $("<div>", {id: user, class: sideDiv});
 	$div[0] = $("<div>", {class: "col-xs-6"})
-	.html($("<img>", {class: "img-responsive"+(bit ?" flip":""),src:"static/img/"+types[user-1]+"_normal.png", style: "width: 100%;"}));
+	.html($("<img>", {class: "img-responsive"+(bit ?" flip":""),src:"static/img/"+ms[user]+"_normal.png", style: "width: 100%;"}));
 	$div[1] = $("<div>", {class: "message col-xs-6"})
 		.html($("<div>", {class: sideMessage, style: "background-color: " + colors[user-1]}));
 	$person.append($div[1-bit]).append($div[bit]);
@@ -52,20 +31,21 @@ var render_msg = function (user, msg, emotion){
 		return;
 	}
 	$("#" + user).find("div .qtime").remove();
-	$('#lines').append($('<p>').append('<b>' + types[user-1] + ': </b>').append($('<em>').text(msg)));
+	$('#lines').append($('<p>').append('<b>' + ms[user] + ': </b>').append($('<em>').text(msg)));
 	tag = (new Date()).getTime();
 	$span = $("<span tag='" + tag + "'>").html(msg+"<br/>");
 	$("#" + user + " .message > div").append($span);
-	if (emotion) $("#" + user +"> div>img").attr('src','static/img/'+types[(user-1)%5]+'_' + emotion+'.png');
+	if (emotion) $("#" + user +"> div>img").attr('src','static/img/'+ms[user]+'_' + emotion+'.png');
 }
 
-var declare_users = function(users){
-	for (var i=0; i<users.length; i++)
-		render_user(users[i]);
+var declare_users = function(){
+	for (var i=0; i<msid.length; i++)
+		render_user(msid[i]);
 }
 
-for (var i=1; i<6; i++)
-	render_user(i);
+declare_users();
+// for (var i=1; i<6; i++)
+// 	render_user(i);
 
 var showTime = function(e, t){
 	var qtime = new Date(parseInt(t));
