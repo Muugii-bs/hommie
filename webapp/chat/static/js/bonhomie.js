54da2dc6198b5ae7f49cc6be3720787d0b9ad909
Batchunag Dashdemberel <gtokyo2010@gmail.com>
2015-12-12 11:11:33 +0900

    Online status

--- webapp/chat/static/js/bonhomie.js
+++ webapp/chat/static/js/bonhomie.js
@@ -46,7 +46,10 @@ Plot = function ( stage ) {
     this.message.style.position="relative";
     this.message.style.width = x + unit;
     // this.message.style.top = y + unit;
-    this.message.innerHTML = "Last Message";
+    this.message.innerHTML = "";
+
+    this.stat.setAttribute("class", "stat");
+    this.stat.style.bottom= y +unit;
 
     this.width = x;
     this.height = y;
@@ -88,9 +91,11 @@ Plot = function ( stage ) {
   this.elm = document.createElement( 'div' );
   this.emotion = document.createElement( 'div' );
   this.message = document.createElement( 'div' );
+  this.stat = document.createElement( 'div' );
   // this.emotion.innerHTML = "ETETE";
   this.elm.appendChild(this.emotion);
   this.elm.appendChild(this.message);
+  this.elm.appendChild(this.stat);
 
   this.elm.style.position = 'absolute';
   stage.appendChild( this.elm );
@@ -149,7 +154,7 @@ var render_msg = function (sender, msg, emotion, place){
 
 
 
-var picSize = 150/familySize, //vmin
+var picSize = 130/familySize, //vmin
     centerX = 45 - picSize, //vw
     centerY = 65- picSize, //vh
     plotRadius = 30; //vmin
